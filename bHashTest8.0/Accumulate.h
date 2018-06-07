ffde6800fcb30ad273177db98ea1a09f8baa526f
LuffyWei <878826538@qq.com>
2018-06-07 19:58:33 +0800

    8.0

---
+++ bHashTest8.0/Accumulate.h
@@ -0,0 +1,47 @@
+//
+// Created by weixun on 18-1-2.
+//
+
+#ifndef BHASHTEST8_0_ACCUMULATE_H
+#define BHASHTEST8_0_ACCUMULATE_H
+
+#include <string>
+#include <vector>
+#include "KVBuffer.h"
+
+
+using namespace std;
+
+struct accumulate_node{
+    uint64_t  fk_size;
+    uint64_t kv_num;
+    vector<string> listOfv;
+    accumulate_node(){
+        fk_size = 0;
+        kv_num = 0;
+    }
+};
+
+class Accumulate {
+public:
+    accumulate_node ** accumulate;
+    int sub_file_num;
+    uint64_t bucket_num;
+    int accumulate_num;
+    uint64_t bucket_num_small;
+
+    Accumulate(int sub_file_num, uint64_t bucket_num, int accumulate_num, uint64_t bucket_num_small);
+    uint64_t get_bucket_id(uint64_t murmur_id);
+    uint64_t get_small_bucket_id(uint64_t murmur_id);
+    int get_file_id(uint64_t bucket_id);
+    int fill_accumulate(string k, int vsize, uint64_t murmur_id, uint64_t bucket_id);
+    int transvert_accumulate(int file_no, uint64_t offset);
+    int insert_accumulate(string k, string v, uint64_t murmur_id, uint64_t bucket_id);
+    int flush_accumulate(string base_path, int file_no, char split_kv, char split_vv);
+    int group_accumulate(string base_path, char split_kv);
+    int groupBy_small(base_node * base_small, string k, string v);
+    ~Accumulate();
+};
+
+
+#endif //BHASHTEST8_0_ACCUMULATE_H
---
+++ bHashTest8.0/cmake-build-debug/CMakeFiles/bHashTest8_0.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bHashTest8_0.dir/main.cpp.o"
+  "CMakeFiles/bHashTest8_0.dir/HashBuckets.cpp.o"
+  "CMakeFiles/bHashTest8_0.dir/KVBuffer.cpp.o"
+  "CMakeFiles/bHashTest8_0.dir/murmurhash.cpp.o"
+  "CMakeFiles/bHashTest8_0.dir/CountMin.cpp.o"
+  "CMakeFiles/bHashTest8_0.dir/KeyBuffer.cpp.o"
+  "CMakeFiles/bHashTest8_0.dir/Accumulate.cpp.o"
+  "bHashTest8_0.pdb"
+  "bHashTest8_0"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bHashTest8_0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
